a51b7157313a13ead54b076290d58ae58703a892
yiyuqian0912 <qian73@wisc.edu>
2025-10-12 18:08:15 -0500

    add @see tag

--- src/DoublyLinkedListTest.java
+++ src/DoublyLinkedListTest.java
@@ -6,12 +6,7 @@ import java.util.NoSuchElementException;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DoublyLinkedListTest {
-    /**
-     * Use this to toggle between using the DoublyLinkedList class and your broken classes
-     * You may submit with either true or false.
-     */
     boolean SHOULD_FAIL = true;
-
     private DoublyLinkedList<Integer> list;
 
     @BeforeEach
@@ -27,7 +22,6 @@ public class DoublyLinkedListTest {
     @Test
     public void testGetAtOutOfBoundsThrowsException() {
         if (SHOULD_FAIL) list = new GetAtOutOfBoundsThrowsException<>();
-
         list.addLast(1);
         list.addLast(2);
         assertThrows(IndexOutOfBoundsException.class, () -> list.get(3));
@@ -36,12 +30,11 @@ public class DoublyLinkedListTest {
 
     /**
      * @author Nathan Su
-     * @see
+     * @see GetAtIndex
      */
     @Test
     public void testGetAtIndex() {
         if (SHOULD_FAIL) list = new GetAtIndex<>();
-
         list.addLast(1);
         list.addLast(2);
         assertEquals(1, list.get(0));
@@ -51,12 +44,11 @@ public class DoublyLinkedListTest {
     // Tests for getFirst
     /**
      * @author Yiyu Qian
-     * @see
+     * @see GetNonNullFirst
      */
     @Test
     public void testGetNonNullFirst() {
         if (SHOULD_FAIL) list = new GetNonNullFirst<>();
-
         list.addLast(1);
         list.addLast(2);
         assertEquals(1, list.getFirst());
@@ -64,24 +56,22 @@ public class DoublyLinkedListTest {
 
     /**
      * @author Nathan Su
-     * @see
+     * @see GetFirstThrowsException
      */
     @Test
     public void testGetFirstThrowsException() {
         if (SHOULD_FAIL) list = new GetFirstThrowsException<>();
-
         assertThrows(NoSuchElementException.class, () -> list.getFirst());
     }
 
     // Tests for getLast
     /**
      * @author Yiyu Qian
-     * @see
+     * @see GetNonNullLast
      */
     @Test
     public void testGetNonNullLast() {
         if (SHOULD_FAIL) list = new GetNonNullLast<>();
-
         list.addLast(1);
         list.addLast(2);
         assertEquals(2, list.getLast());
@@ -89,24 +79,22 @@ public class DoublyLinkedListTest {
 
     /**
      * @author Nathan Su
-     * @see
+     * @see GetLastThrowsException
      */
     @Test
     public void testGetLastThrowsException() {
         if (SHOULD_FAIL) list = new GetLastThrowsException<>();
-
         assertThrows(NoSuchElementException.class, () -> list.getLast());
     }
 
     // Tests for isEmpty
     /**
      * @author Yiyu Qian
-     * @see
+     * @see IsEmptyWhenCreated
      */
     @Test
     public void testIsEmptyWhenCreated() {
         if (SHOULD_FAIL) list = new IsEmptyWhenCreated<>();
-
         assertTrue(list.isEmpty());
         list.addLast(1);
         assertFalse(list.isEmpty());
@@ -115,12 +103,11 @@ public class DoublyLinkedListTest {
     // Tests for add
     /**
      * @author Nathan Su
-     * @see
+     * @see AddAtIndexOutOfBoundsThrowsException
      */
     @Test
     public void testAddAtIndexOutOfBoundsThrowsException() {
         if (SHOULD_FAIL) list = new AddAtIndexOutOfBoundsThrowsException<>();
-
         list.addLast(1);
         list.addLast(2);
         assertThrows(IndexOutOfBoundsException.class, () -> list.add(3, 1));
@@ -129,12 +116,11 @@ public class DoublyLinkedListTest {
 
     /**
      * @author Yiyu Qian
-     * @see
+     * @see AddAtLegalIndex
      */
     @Test
     public void testAddAtLegalIndex() {
         if (SHOULD_FAIL) list = new AddAtLegalIndex<>();
-
         list.add(0, 1);
         assertEquals(1, list.getFirst());
         list.add(1, 2);
@@ -146,12 +132,11 @@ public class DoublyLinkedListTest {
     // Tests for addFirst
     /**
      * @author Nathan Su
-     * @see
+     * @see AddFirstAddsElement
      */
     @Test
     public void testAddFirstAddsElement() {
         if (SHOULD_FAIL) list = new AddFirstAddsElement<>();
-
         list.addFirst(1);
         list.addFirst(2);
         assertEquals(2, list.getFirst());
@@ -161,12 +146,11 @@ public class DoublyLinkedListTest {
     // Tests for addLast
     /**
      * @author Yiyu Qian
-     * @see
+     * @see AddLastAddsElement
      */
     @Test
     public void testAddLastAddsElement() {
         if (SHOULD_FAIL) list = new AddLastAddsElement<>();
-
         list.addLast(1);
         list.addLast(2);
         assertEquals(1, list.getFirst());
@@ -174,149 +158,127 @@ public class DoublyLinkedListTest {
     }
 
     // Tests for remove
-
     /**
      * @author Nathan Su
-     * @see
+     * @see RemoveAtIndex
      */
     @Test
     public void testRemoveAtIndex() {
         if (SHOULD_FAIL) list = new RemoveAtIndex<>();
-
         list.addLast(1);
         list.addLast(2);
         list.addLast(3);
-
         list.remove(2);
-
         assertEquals(2, list.removeLast());
     }
 
     /**
      * @author Yiyu Qian
-     * @see
+     * @see RemoveAtIndexOutOfBoundsException
      */
     @Test
     public void testRemoveAtIndexOutOfBoundsException() {
         if (SHOULD_FAIL) list = new RemoveAtIndexOutOfBoundsException<>();
-
         list.addLast(1);
         list.addLast(2);
-
         assertThrows(IndexOutOfBoundsException.class, () -> list.remove(2));
     }
 
     // Tests for removeFirst
     /**
      * @author Nathan Su
-     * @see
+     * @see RemoveFirst
      */
     @Test
     public void testRemoveFirst() {
         if (SHOULD_FAIL) list = new RemoveFirst<>();
-
         list.addLast(1);
         list.addLast(2);
-
         list.removeFirst();
-
         assertEquals(2, list.removeFirst());
     }
 
     /**
      * @author Yiyu Qian
-     * @see
+     * @see RemoveFirstThrowsException
      */
     @Test
     public void testRemoveFirstThrowsException() {
         if (SHOULD_FAIL) list = new RemoveFirstThrowsException<>();
-
         assertThrows(NoSuchElementException.class, () -> list.removeFirst());
     }
 
     // Tests for removeLast
     /**
      * @author Nathan Su
-     * @see
+     * @see RemoveLast
      */
     @Test
     public void testRemoveLast() {
         if (SHOULD_FAIL) list = new RemoveLast<>();
-
         list.addLast(1);
         list.addLast(2);
         list.addLast(3);
-
         list.removeLast();
-
         assertEquals(2, list.removeLast());
     }
 
     /**
      * @author Yiyu Qian
-     * @see
+     * @see RemoveLastThrowsException
      */
     @Test
     public void testRemoveLastThrowsException() {
         if (SHOULD_FAIL) list = new RemoveLastThrowsException<>();
-
         assertThrows(NoSuchElementException.class, () -> list.removeLast());
     }
 
     // Tests for size
     /**
      * @author Nathan Su
-     * @see
+     * @see SizeEmpty
      */
     @Test
     public void testSizeEmpty() {
         if (SHOULD_FAIL) list = new SizeEmpty<>();
-
         assertEquals(0, list.size());
     }
 
     /**
      * @author Yiyu Qian
-     * @see
+     * @see SizeAfterAdd
      */
     @Test
     public void testSizeAfterAdd() {
         if (SHOULD_FAIL) list = new SizeAfterAdd<>();
-
         list.addLast(1);
-
         assertEquals(1, list.size());
     }
 
     // Tests for set
     /**
      * @author Nathan Su
-     * @see
+     * @see Set
      */
     @Test
     public void testSet() {
         if (SHOULD_FAIL) list = new Set<>();
-
         list.addLast(1);
         list.addLast(2);
         list.addLast(3);
-
         list.set(2, 4);
-
         assertEquals(4, list.get(2));
     }
 
     /**
      * @author Yiyu Qian
-     * @see
+     * @see SetOutOfBoundsException
      */
     @Test
     public void testSetOutOfBoundsException() {
         if (SHOULD_FAIL) list = new SetOutOfBoundsException<>();
-
         list.addLast(1);
         list.addLast(2);
-
         assertThrows(IndexOutOfBoundsException.class, () -> list.set(2, 4));
     }
 }
